2834d0dc65f9a08a0bc8148e43adb867cc316a77
Loubyn Sineus <Loubyn.Sineus@ibm.com>
2022-07-11 11:05:25 -0700

    initial week 5

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express"),
        app = express(),
        port = process.env.PORT || 8080,
        cors = require("cors");
+const { responsiveFontSizes } = require("@mui/material");
 const bodyParser = require('body-parser');
 const fs = require("fs");
 
@@ -13,9 +14,11 @@ app.get("/", (req, res) => {
     res.send({ message: "Connected to Backend server!" });
     });
 
+//add new item to json file
 app.post("/add/item", addItem)
 
 function addItem (request, response) {
+    // Converting Javascript object (Task Item) to a JSON string
     let id = request.body.jsonObject.id
     let task = request.body.jsonObject.task
     let curDate = request.body.jsonObject.currentDate
@@ -37,3 +40,17 @@ function addItem (request, response) {
     });
     response.send(200)
     }
+
+app.get("/get/items", getItems)
+//** week5, get all items from the json database*/
+  function getItems (request, response) {
+    //begin here
+    
+  } 
+
+app.get("/get/searchitem",searchItems)
+//**week 5, search items service */
+  function searchItems (request, response) {
+    //begin here
+    
+  }
